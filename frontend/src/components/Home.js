6b2056e064854715a39401703f32d5d54381cbe9
bhuvan <bhuvanchau@gmail.com>
2022-11-06 09:44:36 -0500

    display backend data on front end

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,14 +4,29 @@ import axios from "axios";
 // eslint-disable-next-line
 import { useFetchers } from "react-router-dom";
 // eslint-disable-next-line
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // eslint-disable-next-line
-const url = "http://localhost:5000/read"
+const url = "http://localhost:5000/read";
 const Home = () => {
-  
+    const [items, setItems] = useState([]);
+
+
+    const fetch = () => {
+        axios
+          .get("http://localhost:5000/read")
+          .then((response) => setItems(response.data));
+    };
+    
+    // component did mount
+    useEffect(() => {
+       fetch();
+    }, []);
+    
+
   return (
-    <div>
-      <h1>Tuder</h1>
+    <div className="app">
+    <h1>TUDOR</h1>
+   {items.length > 0 && items.map((item) => <p key={item.user}>{item.user}, {item.object}</p>)}
     </div>
   );
 };
